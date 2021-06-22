d3fe966c88c33d49ece209ff3be8b27a740b93d5
Dreeedy <dreeedyone@gmail.com>
2021-06-22 12:15:28 +0300

    feat: реализовал глобальный перехват клавиш
    реализовано по примеру https://github.com/kfirprods/NonInvasiveKeyboardHook

--- Hypersphere/App.xaml.cs
+++ Hypersphere/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,6 +16,7 @@ namespace Hypersphere
     public partial class App : Application
     {
         private TaskbarIcon notifyIcon;
+        private static KeyboardHookManager keyboardHookManager;
 
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -22,12 +24,33 @@ namespace Hypersphere
 
             // создаем значок уведомления (это ресурс, объявленный в NotifyIconResources.xaml
             notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+            this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            keyboardHookManager = new KeyboardHookManager();
+            keyboardHookManager.Start();
+
+            RegisterHotkeyAsync();
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
-            notifyIcon.Dispose(); // значок очистится автоматически, но это чище
+            notifyIcon.Dispose(); // очистить память от значка
             base.OnExit(e);
+        }
+
+        static void PerformRegisterHotkey()
+        {
+            // 0x2C - print screen virutal code
+            keyboardHookManager.RegisterHotkey(ModifierKeys.Control, 0x2C, () =>
+            {
+                Debug.WriteLine("Ctrl+Print Screen detected");
+            });
+
+        }
+
+        static async void RegisterHotkeyAsync()
+        {  
+            await Task.Run(() => PerformRegisterHotkey());
         }
     }
 }
